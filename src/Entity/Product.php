af033bd13eabe53ff06165875d29693428a0264f
Benjamin Dujardin <benjamin.preuve@gmail.com>
2023-12-08 16:54:41 +0100

    ajout getter setter caracteristiques product

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -25,4 +25,76 @@ class Product
 
     /** nom de l'image */
     private ?string $imageName;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): Product
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): Product
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    public function getCreatedAt(): \DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): Product
+    {
+        $this->createdAt = $createdAt;
+        return $this;
+    }
+
+    public function getQuantityInStock(): ?int
+    {
+        return $this->quantityInStock;
+    }
+
+    public function setQuantityInStock(?int $quantityInStock): Product
+    {
+        $this->quantityInStock = $quantityInStock;
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(?float $price): Product
+    {
+        $this->price = $price;
+        return $this;
+    }
+
+    public function getImageName(): ?string
+    {
+        return $this->imageName;
+    }
+
+    public function setImageName(?string $imageName): Product
+    {
+        $this->imageName = $imageName;
+        return $this;
+    }
+
 }
