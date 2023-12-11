a1cd808919fad9d1b1aca5b82eab168c1516ca45
Benjamin Dujardin <benjamin.preuve@gmail.com>
2023-12-11 15:28:06 +0100

    un produit par page

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -4,14 +4,16 @@ namespace App\Controller;
 
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProductController extends AbstractController
 {
 /** contrôleur qui sert une page contenant la liste de tous les produits */
 
-    #[Route(path: '/product/show-all', name: 'product_show_all')]
-    public function showall(ProductRepository $productRepository){
+    #[Route('/product/show-all', name: 'product_show_all')]
+    public function showall(ProductRepository $productRepository):Response{
 
         //il faut recuperer tous les produits de la base de donnees
         $products = $productRepository->findAll();
@@ -21,5 +23,21 @@ class ProductController extends AbstractController
         //il faut retourner cette page
         return $this->render('base.html.twig',
         ['products'=> $products]);
+    }
+
+    /** contrôleur qui sert une page contenant la fiche d'un produit */
+    #[Route('/product/show/{id}', name: 'product_show', requirements: ['id' =>'\d+'])]
+    public function show(int $id, ProductRepository $productRepository):Response {
+
+        //recuperation du produit dont l'id a été passé à notre contrôleur
+        $product = $productRepository->find($id);
+
+        if (null == $product){
+            throw new NotFoundHttpException('Ce produit n\'existe pas');
+        }
+
+        //il faut construire la page html avec le produit recupere
+        return $this->render('product/product_show.html.twig',
+        ['product'=>$product]);
     }
 }
